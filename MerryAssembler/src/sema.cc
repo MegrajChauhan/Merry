d013a574592c27a46715e3ed4581c1a93cea0bfc
luccie-cmd <hoedje201@gmail.com>
2024-02-03 15:47:24 +0100

    Added more math as well as altering the program counter

--- MerryAssembler/src/sema.cc
+++ MerryAssembler/src/sema.cc
@@ -22,31 +22,46 @@ std::vector<std::string> valid_registers = {
 
 std::vector<merry::front_end::AstInstType> first_op_has_register = {
     merry::front_end::AstInstType::ADD,
+    merry::front_end::AstInstType::SUB,
+    merry::front_end::AstInstType::MUL,
+    merry::front_end::AstInstType::DIV,
+    merry::front_end::AstInstType::MOD,
 };
 
 merry::front_end::AstNodeInst merry::front_end::Sema::resolve_inst_imm_or_reg(AstNodeInst inst){
     AstNodeInst copy_inst = inst;
-    if(copy_inst.get_operands().size() == 0){
-        return inst;
-    }
-    if(std::find(first_op_has_register.begin(), first_op_has_register.end(), copy_inst.get_inst_type()) != first_op_has_register.end()){
-        if(std::find(valid_registers.begin(), valid_registers.end(), copy_inst.get_operands().at(0)) == valid_registers.end()){
-            std::cout << "Encountered an invalid register name `" << copy_inst.get_operands().at(0) << "`\n";
+    // Check if it's not a jump or call instruction
+    if(inst.get_inst_type() != AstInstType::JMP && inst.get_inst_type() != AstInstType::CALL){
+        if(copy_inst.get_operands().size() == 0){
             return inst;
         }
-    }
-    if(std::find(valid_registers.begin(), valid_registers.end(), copy_inst.get_operands().at(1)) != valid_registers.end()){
-        copy_inst.is_second_reg = true;
-    }
-    if(copy_inst.is_second_reg){
-        if(std::find(valid_registers.begin(), valid_registers.end(), copy_inst.get_operands().at(1)) == valid_registers.end()){
-            std::cout << "Encountered an invalid register name `" << copy_inst.get_operands().at(1) << "`\n";
-            return inst;
+        if(std::find(first_op_has_register.begin(), first_op_has_register.end(), copy_inst.get_inst_type()) != first_op_has_register.end()){
+            if(std::find(valid_registers.begin(), valid_registers.end(), copy_inst.get_operands().at(0)) == valid_registers.end()){
+                std::cout << "Encountered an invalid register name `" << copy_inst.get_operands().at(0) << "`\n";
+                exit(1);
+            }
         }
+        if(std::find(valid_registers.begin(), valid_registers.end(), copy_inst.get_operands().at(1)) != valid_registers.end()){
+            copy_inst.is_second_reg = true;
+        }
+        if(copy_inst.is_second_reg){
+            if(std::find(valid_registers.begin(), valid_registers.end(), copy_inst.get_operands().at(1)) == valid_registers.end()){
+                std::cout << "Encountered an invalid register name `" << copy_inst.get_operands().at(1) << "`\n";
+                exit(1);
+            }
+        }
+    } else{
+        unresolved_jmps.push_back({copy_ast.get_insts().size(), inst.get_operands().at(0)});
     }
+    insts_size++;
     return copy_inst;
 }
 merry::front_end::AstNodeLabel merry::front_end::Sema::resolve_label_addr(AstNodeLabel label){
-    label.set_addr(copy_ast.get_insts().size());
+    if(find_dub_label(label.get_name())){
+        std::cout << "Cannot redifine label " << label.get_name() << std::endl;
+        exit(1);
+    }
+    insts_size++;
+    label.set_addr(insts_size);
     return label;
 }
