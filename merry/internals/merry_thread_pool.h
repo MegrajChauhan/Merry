dbc8287d836953048ca1d4bd03f9fa1c2137363d
MegarjChauhan <megrajchauhan123@gmail.com>
2024-02-09 21:31:56 +0545

    Unsurmountable pain

--- merry/internals/merry_thread_pool.h
+++ merry/internals/merry_thread_pool.h
@@ -11,22 +11,48 @@
 typedef struct MerryThreadPoolThread MerryThreadPoolThread;
 typedef struct MerryThreadPool MerryThreadPool;
 typedef enum MerryThreadState MerryThreadState;
+typedef enum MerryThreadType MerryThreadType;
+typedef union MerryThPoolExec MerryThPoolExec;
+typedef union MerryThPoolExecArg MerryThPoolExecArg;
 
 enum MerryThreadState
 {
-    WORKING,
+    WORKING = 5,
     FREE,
-    NOT_INIT,
 };
 
-_MERRY_DEFINE_FUNC_PTR_(mptr_t, merry_pool_exec_t, mptr_t, ...)
+enum MerryThreadType
+{
+    INP,
+    OUT,
+};
+
+_MERRY_DEFINE_FUNC_PTR_(mptr_t, merry_IO_exec_t, mptr_t, maddress_t)
+
+union MerryThPoolExec
+{
+    merry_IO_exec_t exec_IO;
+};
+
+union MerryThPoolExecArg
+{
+    struct
+    {
+        mptr_t mem;
+        maddress_t addr;
+    } IOarg_t;
+};
 
 struct MerryThreadPoolThread
 {
     MerryThread *thread;
     mbool_t stop;           // tell the thread to stop
+    mbool_t _is_init;       // is the thread initialized?
     MerryThreadState state; // the state of the thread
-    merry_pool_exec_t exec_func;
+    MerryThreadType type;   // the type of operation the thread is performing
+    MerryThPoolExec exec_func;
+    MerryThPoolExecArg arg;
+    MerryCond *pool_cond; // the pool's cond
     // Mutexes and condition variables for each thread in the pool could be added
 };
 
@@ -35,9 +61,16 @@ struct MerryThreadPool
     MerryThreadPoolThread *threads; // the pool of the threads
     msize_t pool_size;              // the pool's size
     msize_t free_thread_count;      // How many threads are free?
+    MerryCond *cond;                // the pool's cond
 };
 
 MerryThreadPool *merry_init_thread_pool(msize_t pool_size);
+
+mptr_t merry_thPool_exec_func(mptr_t thread);
+
+mret_t merry_thPool_start_thread(MerryThreadPool *pool, msize_t id);
+
+mret_t merry_assign_thread(MerryThPoolExec exec_func, MerryThPoolExecArg args);
 
 void merry_destroy_thread_pool(MerryThreadPool *pool);
 
--- merry/merry_thread_pool.c
+++ merry/merry_thread_pool.c
@@ -11,6 +11,12 @@ MerryThreadPool *merry_init_thread_pool(msize_t pool_size)
         free(pool);
         return RET_NULL;
     }
+    if ((pool->cond = merry_cond_init()) == RET_NULL)
+    {
+        free(pool->threads);
+        free(pool);
+        return RET_NULL;
+    }
     pool->free_thread_count = pool_size; // all the threads are free
     pool->pool_size = pool_size;
     return pool;
@@ -32,12 +38,83 @@ _MERRY_INTERNAL_ void merry_thPool_kill_threads(MerryThreadPool *pool)
     }
 }
 
+mptr_t merry_thPool_exec_func(mptr_t thread)
+{
+    MerryThreadPoolThread *th = (MerryThreadPoolThread *)thread;
+    // now this thread will run
+    while (mtrue)
+    {
+        if (th->stop == mtrue)
+        {
+            break;
+        }
+        // wait until we are assigned a job
+        while (atomic_load(&th->state) != WORKING)
+        {
+        }
+
+        // check if we need to stop
+        if (th->stop == mtrue)
+        {
+            break;
+        }
+
+        // if we get here then it means that we were assigned a job
+        switch (th->type)
+        {
+        case INP:
+        {
+            // this is an input operation
+            th->exec_func.exec_IO(th->arg.IOarg_t.mem, th->arg.IOarg_t.mem); // now we expect the service function to set the correct registers for success indication
+            break;
+        }
+        case OUT:
+        {
+            th->exec_func.exec_IO(th->arg.IOarg_t.mem, th->arg.IOarg_t.addr);
+            break;
+        }
+        }
+        // the job is done and the thread is free once again
+        th->state = FREE;
+        merry_cond_signal(th->pool_cond); // tell the pool that it can accept more threads
+    }
+    return RET_NULL;
+}
+
+mret_t merry_thPool_start_thread(MerryThreadPool *pool, msize_t id)
+{
+    if ((pool->threads[id].thread = merry_thread_init()) == RET_NULL)
+        return RET_FAILURE;
+    pool->threads[id]._is_init = mtrue;
+    pool->threads[id].state = FREE;
+    pool->threads[id].stop = mfalse;
+    if (merry_create_detached_thread(pool->threads[id].thread, &merry_thPool_exec_func, &pool->threads[id]) == RET_FAILURE)
+        return RET_FAILURE;
+    pool->free_thread_count--;
+    return RET_SUCCESS;
+}
+
+_MERRY_INTERNAL_ msize_t merry_thPool_get_free_thread(MerryThreadPool *pool)
+{
+    // if there are none, we simply
+}
+
+mret_t merry_assign_thread(MerryThPoolExec exec_func, MerryThPoolExecArg args)
+{
+    // assign a free thread a new task
+}
+
 void merry_destroy_thread_pool(MerryThreadPool *pool)
 {
     if (pool == NULL)
         return;
     // we have to stop every thread as well
     merry_thPool_kill_threads(pool);
+    for (msize_t i = 0; i < pool->pool_size; i++)
+    {
+        merry_thread_destroy(pool->threads[i].thread);
+    }
+    merry_cond_destroy(pool->cond);
     free(pool->threads);
     free(pool);
 }
