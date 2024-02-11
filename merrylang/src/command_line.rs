b9350d3ba5a6068ee5ad95794ff12e3392a42735
MegarjChauhan <megrajchauhan123@gmail.com>
2024-02-11 21:02:12 +0545

    Minor bug fixes

---
+++ merrylang/src/command_line.rs
@@ -0,0 +1,17 @@
+use std::fs::File; // for file IO
+use std::collections::String; // strings
+use std::collections::Vector; // vectors for all arguments
+
+// we expect the ending extension for the input file to be ".merry"
+
+enum Args_t
+{
+  _FILE_NAME, // the file provided[must follow 
+}
+
+// represents the command line options
+struct CommandLine
+{
+  _args_: Vec<String>,
+  // now for each option we expect
+}
