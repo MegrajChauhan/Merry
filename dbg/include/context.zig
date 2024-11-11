334d0faf88682a8f7ef5c378ddd7f43a0b33ceae
Megraj Chauhan <megrajchauhan123@gmail.com>
2024-11-11 11:06:29 +0545

    Zig is complex

--- dbg/include/context.zig
+++ dbg/include/context.zig
@@ -1,53 +1,97 @@
-const defs = @import("defs.zig");
-const utils = @import("utils.zig");
-const queue = @import("queue.zig");
-const fs = defs.std.fs;
-const thread = defs.std.Thread;
-const array = defs.std.ArrayList;
+const std = @import("std");
+const fs = std.fs;
+const array = std.ArrayList;
 
-// Architecture:
-// One central thread that will manage everything.
-// This central thread will take in input from the USER on what to do.
-// One thread per OS will run, waiting for commands from the central thread
-// Each worker thread will be a 'context' of that specific OS
-// Each thread will wait for any command from the central thread and based on it, send requests to the OS.
-// There will not be any kind of waiting on the worker threads other than waiting for commands.
-
-fn ChildContext() type {
+pub fn ChildContext() type {
     return struct {
         const TYPE = @This();
-        // THREAD: thread.Handle, // maybe we won't need this
         OUT_STREAM: fs.File,
         IN_STREAM: fs.File,
 
-        fn init(self: TYPE, INPUT: fs.File, OUTPUT: fs.File) TYPE {
-            self.IN_STREAM = INPUT;
-            self.OUT_STREAM = OUTPUT;
-            return self;
+        pub fn init(INPUT: fs.File, OUTPUT: fs.File) TYPE {
+            return ChildContext(){ .IN_STREAM = INPUT, .OUT_STREAM = OUTPUT };
         }
 
-        fn deinit(self: TYPE) void {
+        pub fn deinit(self: *TYPE) void {
             self.IN_STREAM.close();
             self.OUT_STREAM.close();
         }
     };
 }
 
-fn Context() type {
+pub fn Context() type {
     return struct {
         const Self = @This();
-        child_context_list: array(@TypeOf(ChildContext())),
+        child_context_list: array(ChildContext()),
 
-        fn init(self: Self) Self
-        {
-            self.child_context_list.init(defs.std.heap.GeneralPurposeAllocator(.{}));
-            return self;
-        }  
+        pub fn init(allocator: std.mem.Allocator) Self {
+            return Context(){ .child_context_list = array(ChildContext()).init(allocator) };
+        }
 
-        fn deinit(self: Self) void
-        {
+        pub fn deinit(self: *Self) void {
             self.child_context_list.deinit();
         }
-        
+
+        pub fn event_loop(self: *Self) !void {
+            self.deinit();
+            const stdin = std.io.getStdIn();
+            const stdout = std.io.getStdOut();
+            const allocator = std.heap.page_allocator;
+
+            var keep_running = true;
+            while (keep_running) {
+                try stdout.writer().print(">>> ", .{});
+
+                // Read command input from the user
+                const command: ?[]u8 = try stdin.reader().readUntilDelimiterOrEofAlloc(allocator, '\n', 256);
+                defer allocator.free(command.?);
+
+                const State = enum {
+                    Start,
+                    ParseFlag,
+                    ProcessCommand,
+                };
+
+                var state = State.Start;
+                var flag_name = std.ArrayList(u8).init(allocator);
+                defer flag_name.deinit();
+
+                var i: usize = 0;
+                while (true) {
+                    const c = command.?.ptr[i];
+                    switch (state) {
+                        State.Start => {
+                            if (c != ' ') {
+                                state = State.ParseFlag;
+                            } else {
+                                i += 1;
+                            }
+                        },
+                        State.ParseFlag => {
+                            if (std.ascii.isAlphanumeric(c)) {
+                                try flag_name.append(c);
+                                i += 1;
+                            } else if ((c == ' ') or (i == (command.?.len))) {
+                                state = State.ProcessCommand;
+                            }
+                        },
+                        State.ProcessCommand => {
+                            const flag_str = flag_name.items;
+                            if (std.mem.eql(u8, flag_str, "exit")) {
+                                keep_running = false;
+                                try stdout.writer().print("Received terminating command.?...\n", .{});
+                            } else if (std.mem.eql(u8, flag_str, "help")) {
+                                try stdout.writer().print("Help: Available commands are -help and -exit\n", .{});
+                            } else {
+                                try stdout.writer().print("Unknown command: {s}\n", .{flag_str});
+                            }
+                            break; // Exit loop after processing command
+                        },
+                    }
+                    if (i == command.?.len)
+                        break;
+                }
+            }
+        }
     };
 }
